f500a4dcac52a79418d99b1eaefbc8a835780142
Artem <lebro53@mail.ru>
2023-03-25 17:57:50 +0300

    Написали метод создания массива

--- project/Program.cs
+++ project/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿// Method--------------------------------------------------
+int Promt(string massege)
+{
+	Console.WriteLine(massege);
+	string value = Console.ReadLine();
+	int result = Convert.ToInt32(value);
+	return result;
+}
+string ElemArray(string massege)
+{
+	Console.WriteLine(massege);
+	string value = Console.ReadLine();
+	return value;
+}
+string[] CreateArrayString(int size)
+{
+	string[] array = new string[size];
+	for (int i = 0; i < array.Length; i++)
+	{
+		array[i] = ElemArray("Введите элемент массива: ");
+	}
+	return array;
+}
